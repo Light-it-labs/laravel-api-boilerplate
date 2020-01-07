5db8d298e9ad6008b02765da50cd66902aa03959
Juan <juan@lightit.io>
2020-01-07 13:37:19 -0300

    Add password reset mail

---
+++ app/Mail/PasswordResetRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class PasswordResetRequest extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from(config('mail.from.address'))
+                    ->view('mails.password-reset-request');
+    }
+}
