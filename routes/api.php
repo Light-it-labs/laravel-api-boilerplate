5fcd9b7974b4b10c8a2b0b47a01fab26b30cbc63
Juan vargas <juan@lightit.io>
2020-02-12 15:02:28 -0300

    Fix route service provider namespace bug

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,9 @@ use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\ExampleController;
 
 /* Auth Routes */
-Route::post('login', [AuthController::class, 'login'])->name('api.login');
-Route::post('register', [AuthController::class, 'register'])->name('api.register');
-Route::middleware('auth:api')->get('user', [AuthController::class, 'user'])->name('api.user');
+Route::post('login', AuthController::class . '@login')->name('api.login');
+Route::post('register', AuthController::class . '@register')->name('api.register');
+Route::middleware('auth:api')->get('user', AuthController::class . '@user')->name('api.user');
 
 /* Routes */
-Route::get('example', [ExampleController::class, 'index'])->name('api.example');
+Route::get('example', ExampleController::class . '@index')->name('api.example');
